3acffd54e6587b319b22b4b3bf79fea291f8a02e
Quoc Trung <phaqy010@mymail.unisa.edu.au>
2021-04-18 18:52:59 +0930

    connect backend and frontend

--- backend/app.py
+++ backend/app.py
@@ -2,9 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/api', methods=['GET'])
 def index():
-    return ("Hello From Backend")
+  return {
+    "field1": "Hello",
+    "field2": "From Backend"
+  }
     
 if __name__== '__main__':
     app.run(debug=True, host='0.0.0.0')
