4d86d15a19d0f662d27e4b014895403ddda380bc
Quoc Trung <phaqy010@mymail.unisa.edu.au>
2021-04-29 21:46:38 +0930

    add model for sa database

--- api/app.py
+++ api/app.py
@@ -1,6 +1,13 @@
-from flask import Flask
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+
 
 @app.route('/api', methods=['GET'])
 def index():
