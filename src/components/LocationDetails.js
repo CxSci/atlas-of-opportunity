3f2e4b2c29d54fa040cb3cca1d821456a36291b3
Gerardo Gastaldo <gerardo@toptal.com>
2021-05-07 17:55:42 -0300

    Update LocationDetails to use a lookup

--- src/components/LocationDetails.js
+++ src/components/LocationDetails.js
@@ -2,81 +2,71 @@ import React from "react";
 import PropTypes from "prop-types";
 import ReactTooltip from "react-tooltip";
 import Collapsible from "react-collapsible";
-import BarGraph from "./BarGraph";
 
 function LocationDetails(props) {
-  const {
-    population,
-    income,
-    quartile,
-    inequality,
-    ggp,
-    jr,
-    bgi,
-    sa1_codes,
-  } = props.select;
+  const select = props.select;
+
+  const propsMapping = [
+    {
+      title: 'Demographic Summary',
+      content: [
+        { id: 'population', label: 'Population' },
+        { id: 'income', label: 'Median Income' },
+      ]
+    },
+    {
+      title: 'Economic Summary',
+      content: [
+        { id: 'quartile', label: 'Income Quartile' },
+        { id: 'inequality', label: 'Inequality % (lower is better)' },
+        { id: null, label: 'Visitor time spent by quartile' },
+      ]
+    },
+    {
+      title: 'Growth Summary',
+      content: [
+        { id: 'ggp', label: 'GDP Growth Potential', desc: 'Economic growth is an increase in the production of economic goods and services,compared from one period of time to another... Traditionally, aggregate economic growth is measured in terms of gross national product (GNP) or gross domestic product (GDP), although alternative metrics are sometimes used.' },
+        { id: 'jr', label: 'Job Resilience', desc: 'The ability to adjust to career change as it happens and,by extension, adapt to what the market demands.' },
+        { id: 'bgi', label: 'Business Growth Index', desc: 'The growth rate is the measure of a company’s increase in revenue and potential to expand over a set period.' },
+        { id: 'sa1_codes', label: 'Included SA1 Regions' },
+      ]
+    }
+  ];
+
+  const renderMetric = (metric) => {
+    let value = select[metric.id];
+    if (typeof value === 'number') {
+      value = Math.floor(value);
+    }
+
+    return (
+      <div>
+        <h2 data-tip data-for={metric.id}>{metric.label}</h2>
+        {metric.desc && (
+          <ReactTooltip id={metric.id}>
+            <strong>{metric.label}</strong>
+            <div style={{maxWidth: 400}}>
+              {metric.desc}
+            </div>
+          </ReactTooltip>
+        )}
+        <p>{value}</p>
+      </div>
+    )
+  }
 
   return (
     <div
       style={{ overflowY: "auto" }}
       className={`sidebar-content`}
     >
-      <Collapsible trigger="Demographic Summary">
-        <h2>Population</h2>
-        <p>{population}</p>
-        <h2>Median Income</h2>
-        <p>{income}</p>
-      </Collapsible>
-
-      <Collapsible trigger="Economic Summary">
-        <h2>Income Quartile</h2>
-        <p>{quartile}</p>
-        <h2> Inequality (lower is better)</h2>
-        <p>{Math.floor(inequality)}%</p>
-        <h2>Visitor time spent by quartile</h2>
-        <div>
-          <BarGraph width={200} height={120} />
-        </div>
-      </Collapsible>
-
-      <Collapsible trigger="Growth Summary ">
-        <h2 data-tip data-for="GDPTip">
-          GDP Growth Potential
-      </h2>
-        <ReactTooltip id="GDPTip">
-          <b> GDP Growth Potential </b> <br />
-        Economic growth is an increase in the production <br />
-        of economic goods and services,compared from <br /> one period of
-        time to another...Traditionally, aggregate <br />
-        economic growth is measured in terms of gross national <br />
-        product (GNP) or gross domestic product (GDP), although
-        <br /> alternative metrics are sometimes used.
-      </ReactTooltip>
-        <p>{ggp}</p>
-        <h2 data-tip data-for="jobTip">
-          Job Resilience
-      </h2>
-        <ReactTooltip id="jobTip">
-          <b> Job Resilience </b> <br />
-        The ability to adjust to career change as it happens <br />
-        and,by extension, adapt to what the market demands.
-      </ReactTooltip>
-        <p>{jr}</p>
-        <h2 data-tip data-for="bgiTip">
-          Business Growth Index
-      </h2>
-        <ReactTooltip id="bgiTip">
-          <b> Business Growth Index </b> <br />
-        The growth rate is the measure of a company’s increase <br />
-        in revenue and potential to expand over a set period.
-      </ReactTooltip>
-        <p>{bgi}</p>
-        <h2 data-tip data-for="SATip">
-          {" "}
-        Included SA1 Regions
-      </h2>
-        <p>{sa1_codes}</p>
-      </Collapsible>
+      {propsMapping.map((section) => (
+        <Collapsible trigger={section.title} key={section.title}>
+          {section.content.map((metric) => (
+            renderMetric(metric)
+          ))}
+        </Collapsible>
+      ))}
     </div>
   )
 }
