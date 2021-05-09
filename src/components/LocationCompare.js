7c560225ed15d8b12db89315ae1b1c467a1d11b6
Ethan Tibbetts <tibblist@gmail.com>
2021-05-08 22:35:42 -0500

    add show comparison button

--- src/components/LocationCompare.js
+++ src/components/LocationCompare.js
@@ -7,6 +7,7 @@ import { removeComparisonFeature } from '../redux/action-creators';
 const root = {
     display: "flex",
     flexDirection: "column",
+    justifyContent: "center",
     width: "100%"
 }
 
@@ -22,6 +23,45 @@ const closeIcon = {
     cursor: "pointer"
 }
 
+const buttonRoot = {
+    "width": "161px",
+    "height": "36px",
+    "left": "79.5px",
+    "top": "0px",
+    "background": "#FFFFFF",
+    "border": "2px solid #000000",
+    "boxSizing": "border-box",
+    "borderRadius": "100px",
+    margin: "auto",
+    marginTop: 15,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    cursor: "pointer"
+  }
+
+const buttonText = {
+    "fontFamily": "Roboto",
+    "fontSize": "14px",
+    "fontStyle": "normal",
+    "fontWeight": "500",
+    "lineHeight": "16px",
+    "letterSpacing": "0em",
+    "textAlign": "center"
+  }
+
+const disclaimerText = {
+    "fontFamily": "Roboto",
+    "fontSize": "14px",
+    "fontStyle": "normal",
+    "fontWeight": "400",
+    "lineHeight": "16px",
+    "letterSpacing": "0em",
+    "textAlign": "center",
+    color: "#666666",
+    marginTop: 10
+  }
+
 const LocationCompare = () => {
     const comparisonFeatures = useSelector(getComparisonFeatures);
     return <div style={root}>
@@ -36,6 +76,12 @@ const LocationCompare = () => {
                 }}/>
             </div>
         })}
+        <div style={buttonRoot}>
+            <p style={buttonText}>Show Comparison</p>
+        </div>
+        <div>
+            <p style={disclaimerText}>Add up to 4 regions.</p>
+        </div>
     </div>
 }
 
