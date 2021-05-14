56689ffdade33d4ab0f47a53eece2231d0a1f6f0
Quoc Trung <phaqy010@mymail.unisa.edu.au>
2021-05-14 10:09:28 +0930

    added geometry to /api/features.geojson endpoint

--- api/app.py
+++ api/app.py
@@ -27,7 +27,7 @@ def insertDataToDatabase():
         df = json.load(file)
 
         for feature in df['features']:
-            cur.execute("""INSERT INTO sa2data VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)""",
+            cur.execute("""INSERT INTO sa2data VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)""",
                         (feature['properties']['SA2_MAIN16'],
                          feature['properties']['SA2_5DIG16'],
                          feature['properties']['SA2_NAME16'],
@@ -39,9 +39,9 @@ def insertDataToDatabase():
                          feature['properties']['GCC_NAME16'],
                          feature['properties']['STE_CODE16'],
                          feature['properties']['STE_CODE16'],
-                         feature['properties']['AREASQKM16']))
+                         feature['properties']['AREASQKM16'],
+                         json.dumps(feature['geometry'])))
             con.commit()
-
 
 insertDataToDatabase()
 
@@ -58,17 +58,16 @@ def test_health():
 
 @app.route('/api/features.geojson', methods=['GET'])
 def get_sa_data():
-    command = "SELECT  j.SA2_MAIN16, j.SA2_5DIG16, j.SA2_NAME16, j.SA3_CODE16, j.SA3_NAME16, j.SA4_CODE16,"
-    command += " j.SA4_NAME16, j.GCC_CODE16, j.GCC_NAME16, j.STE_CODE16, j.STE_NAME16, j.AREASQKM16,"
-    command += " persons_num, median_persons_age, males_num, median_male_age, females_num, median_female_age,"
-    command += " percentage_person_aged_0_14, percentage_person_aged_15_64, percentage_person_aged_65_plus, earners_persons,"
-    command += " median_age_of_earners_years, gini_coefficient_no, median_aud, mean_aud, income_aud, highest_quartile_pc,"
-    command += " income_share_top_5pc, third_quartile_pc, lowest_quartile_pc, income_share_top_1pc, second_quartile_pc,"
-    command += " income_share_top_10pc,popfraction,quartile,occup_diversity,bsns_entries,bsns_exits,bsns_growth_rate,bridge_diversity,"
-    command += " bridge_rank1,bridge_rank2,bridge_rank3,bridge_rank4,bridge_rank5,time_q1,time_q2,time_q3,time_q4,tot_time,"
-    command += " fq1,fq2,fq3,fq4,income_diversity,raw_inequality,inequality,inflow_r1,inflow_r2,inflow_r3,outflow_r1,outflow_r2,outflow_r3,"
-    command += " spent_r1,spent_r2,spent_r3,gain_r1,gain_r2,gain_r3,exchanged_r1,exchanged_r2,exchanged_r3, j.geometry"
-    command += " FROM sadata c INNER JOIN sa2data j ON c.SA2_code = j.SA2_MAIN16"
+
+    command = """SELECT j.* ,persons_num, median_persons_age, males_num, median_male_age, females_num, median_female_age,
+        percentage_person_aged_0_14, percentage_person_aged_15_64, percentage_person_aged_65_plus, earners_persons,
+        median_age_of_earners_years, gini_coefficient_no, median_aud, mean_aud, income_aud, highest_quartile_pc,
+        income_share_top_5pc, third_quartile_pc, lowest_quartile_pc, income_share_top_1pc, second_quartile_pc,
+        income_share_top_10pc,popfraction,quartile,occup_diversity,bsns_entries,bsns_exits,bsns_growth_rate,bridge_diversity,
+        bridge_rank1,bridge_rank2,bridge_rank3,bridge_rank4,bridge_rank5,time_q1,time_q2,time_q3,time_q4,tot_time,
+        fq1,fq2,fq3,fq4,income_diversity,raw_inequality,inequality,inflow_r1,inflow_r2,inflow_r3,outflow_r1,outflow_r2,outflow_r3,
+        spent_r1,spent_r2,spent_r3,gain_r1,gain_r2,gain_r3,exchanged_r1,exchanged_r2,exchanged_r3
+        FROM sadata c INNER JOIN sa2data j ON c.SA2_code = j.SA2_MAIN16"""
 
     dict_cur.execute(command)
     rows = dict_cur.fetchall()
@@ -89,14 +88,15 @@ def get_sa_data():
         feature = {
             "type": "Feature",
             "properties": {},
-            "geometry": "Not added yet"
+            "geometry": {}
         }
 
         for k in row.keys():
             if k != 'geometry':
                 feature["properties"][CheckColName(k)] = row[k]
-            # else:
-            #     feature["geometry"][k] = row[k]
+            else:
+                feature["geometry"] = json.loads(row[k])
+
 
         result["features"].append(feature)
     return jsonify(result)
@@ -109,6 +109,6 @@ def CheckColName(col: str) -> str:
         return col.upper()
     else:
         return col
-
+# remove
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
