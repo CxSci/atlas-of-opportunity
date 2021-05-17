9ea6483ebd6b72a9c4ef66fb8a0f223ccfabc7dd
Gerardo Gastaldo <gerardo@toptal.com>
2021-05-17 13:03:43 -0300

    Add inline comparison for each metric

--- src/components/LocationDetails.js
+++ src/components/LocationDetails.js
@@ -3,32 +3,16 @@ import PropTypes from "prop-types";
 import ReactTooltip from "react-tooltip";
 import propsMapping from "./propsMapping";
 import CollapsibleSection from "./CollapsibleSection";
+import { formatValue } from "../utils/formatValue";
 
 const LocationDetails = (props) => {
   const featureProps = props.feature.properties;
-  
+  const comparisonFts = props.comparison;
+  const allFeatures = [...comparisonFts, props.feature].filter((val, idx, arr) => arr.indexOf(val) === idx);
+
   const renderMetric = (metric) => {
-    let value = featureProps[metric.id];
-    switch (metric.format) {
-      case 'number':
-        if (value > 100) {
-          value = Math.floor(value)
-          value = value.toLocaleString('en-US');
-        }
-        else {
-          value = Math.floor(value * 10000) / 10000;
-          value = value.toLocaleString('en-US', { minimumFractionDigits: 4 });
-        }
-        break;
-      case 'currency':
-        value = value.toLocaleString(undefined, { style: "currency", currency: "AUS" });
-        break;
-      case 'percent':
-        value = `${Math.floor(value)}%`;
-        break;
-      default:
-        break;
-    }
+    let rawValue = featureProps[metric.id];
+    const value = formatValue(rawValue, metric.format);
 
     return (
       <div key={metric.id}>
@@ -41,8 +25,26 @@ const LocationDetails = (props) => {
             </div>
           </ReactTooltip>
         )}
-        <p>{value}</p>
+        {comparisonFts.length ? (
+          allFeatures.map(ft => 
+            renderValue(ft, metric)
+          )
+        ) : (
+          <p>{value}</p>
+        )}
       </div>
+    )
+  }
+
+  const renderValue = (feature, metric) => {
+    let rawValue = feature.properties[metric.id];
+    const value = formatValue(rawValue, metric.format);
+    
+    return (
+      <p key={feature.id}>
+        <span>{feature.properties.SA2_NAME16}</span>
+        <span>{value}</span>
+      </p>
     )
   }
 
