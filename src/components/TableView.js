ccbc748c3f687475f6ac6fdea1e6ed7d9cce507c
Gerardo Gastaldo <gerardo@toptal.com>
2021-05-24 12:08:18 -0300

    Refactor TableView with better Table component, support different how height

--- src/components/TableView.js
+++ src/components/TableView.js
@@ -2,66 +2,66 @@ import React from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import Collapsible from "react-collapsible";
-import { Table, Column } from "react-virtualized";
+import Table from "rc-table";
 import propsMapping from "./propsMapping";
 import { formatValue } from "../utils/formatValue";
-import "../css/TableView.css";
+import "../css/TableView.css"
+const { Column } = Table;
 
 const TableView = ({comparisonFeatures}) => {
-  const rowHeight = 30;
-  const nameColumnWidth = 200;
-  const dataColumnWidth = 150;
+  const nameColumnWidth = 170;
+  const dataColumnWidth = 120;
+  const colPadding = 10 * 2;
   const columns = comparisonFeatures.map((ft, idx) => ({ 
     dataKey: `regionData${idx + 1}`,
     label: ft.properties.SA2_NAME16
   }));
   const nameColumnStyle = {
     width: nameColumnWidth,
-    margin: '0 10px 0 30px',
+    margin: '0 10px',
   }
   const dataColumnStyle = {
     width: dataColumnWidth,
-    marginRight: '10px',
+    margin: '0 10px',
   }
   
   const renderTable = (section) => {
     const data = [];
-    const height = section.content.length * rowHeight;
     
-    section.content.forEach(metric => {
+    section.content.forEach((metric, idx) => {
       let columnValues = comparisonFeatures.map((ft, idx) => {
         let rawValue = ft.properties[metric.id];
         const value = formatValue(rawValue, metric.format);
         return ({ [`regionData${idx + 1}`]: value })
       });
       columnValues.push({regionName: metric.label});
+      columnValues.push({id: idx});
       const row = columnValues.reduce((prev, curr) => ({...prev, ...curr}));
       data.push(row);
     })
 
+    const renderCell = (value, record) => {
+      const metric = section.content[record.id];
+      const isChart = metric.type === 'chart';
+      return (
+        !isChart ? value : <div className="fake-chart"></div>
+      );
+    }
+
     return (
       <Table
-        headerHeight={0}
-        disableHeader
-        width={nameColumnWidth + dataColumnWidth * 4 + 60}
-        height={height}
-        rowHeight={rowHeight}
-        rowGetter={({ index }) => data[index]}
-        rowCount={data.length}
-        rowStyle={rowStyle}
+        data={data}
+        rowKey="id"
+        showHeader={false}
+        rowClassName="table-row"
       >
-        <Column label='Name' dataKey='regionName' width={nameColumnWidth} />
+        <Column dataIndex='regionName' width={nameColumnWidth + colPadding} />
         {columns.map(col => (
-          <Column dataKey={col.dataKey} width={dataColumnWidth} key={col.dataKey} />
+          <Column dataIndex={col.dataKey} width={dataColumnWidth + colPadding} key={col.dataKey} render={renderCell} />
         ))}
       </Table>
     )
   }
-
-  const rowStyle = ({index}) => {
-    const color = index % 2 ? '#F5F5F5' : '#FFFFFF';
-    return { backgroundColor: color };
-  };
   
   return (
     <div className="table-view">
