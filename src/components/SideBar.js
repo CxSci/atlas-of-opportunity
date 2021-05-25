ce5c2500c74c9b4c024fc078d4aa30f7a01769b4
Gerardo Gastaldo <gerardo@toptal.com>
2021-05-25 12:37:18 -0300

    Refactor Sidebar as function component, remove commented code

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,4 +1,4 @@
-import React, {Fragment} from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 
@@ -19,32 +19,14 @@ import { Switch, Route } from "react-router";
 import ComparisonSidebarContent from "./ComparisonSidebarContent";
 import CollapsibleSection from "./CollapsibleSection";
 
-class Sidebar extends React.Component {
-  static propTypes = {
-    select: PropTypes.object.isRequired,
-    selectedFeature: PropTypes.object,
-    comparisonFeatures: PropTypes.array.isRequired
-  };
-  
-  render() {
+const Sidebar = (props) => {
     const {
       selectedFeature,
       comparisonFeatures,
-    } = this.props;
-
-    const PanelContainer = (props) => {
-      const featureSelected = this.props.selectedFeature
-        ? "featureSelected"
-        : "noFeatureSelected";
-      return (
-        <div className={`panel-container ${featureSelected}`}>
-          {props.children}
-        </div>
-      )
-    }
+    } = props;
     
-    const isCompared = this.props.comparisonFeatures.find(feature => feature.properties["SA2_MAIN16"] === this.props.selectedFeature?.properties["SA2_MAIN16"]) !== undefined;
-    const enableButton = this.props.comparisonFeatures.length >= 4;
+    const isCompared = comparisonFeatures.find(feature => feature.properties["SA2_MAIN16"] === selectedFeature?.properties["SA2_MAIN16"]) !== undefined;
+    const enableButton = comparisonFeatures.length >= 4;
     
     const comparisonClick = (feature) => {
       if (isCompared) {
@@ -57,30 +39,15 @@ class Sidebar extends React.Component {
     const ActionButtons = () => (
       <div className="actionButtonsContainer">
         <button className="actionButton"><FavoriteIcon className="icon"/> Add to Favorites</button>
-        <button disabled={enableButton} className="actionButton" onClick={() => comparisonClick(this.props.selectedFeature)}>
+        <button disabled={enableButton} className="actionButton" onClick={() => comparisonClick(selectedFeature)}>
           <ComparisonIcon className="icon"/>
           {isCompared ? "Remove from Comparison" : "Add to Comparison"}
         </button>
       </div>
-
     );
-    // const featureDebug = (feature) => {
-    //   if (!feature || !feature.properties) {
-    //     return ""
-    //   }
-    //   return (
-    //     <ul className="sidebar-content">
-    //     {
-    //       Object.keys(feature.properties).map((key) => (
-    //         <li key={key}>{key}: {feature.properties[key]}</li>
-    //       ))
-    //     }
-    //     </ul>
-    //   )
-    // }
 
     return (
-      <PanelContainer>
+      <div className={`panel-container ${selectedFeature ? "featureSelected" : "noFeatureSelected"}`}>
         <SidebarButton />
         <div className={`sidebar-container`}>
           <Switch>
@@ -90,7 +57,7 @@ class Sidebar extends React.Component {
             <Route render={() => (
               <>
                 <SASearchField />
-                {this.props.selectedFeature ?
+                {selectedFeature ?
                   <>
                     <ActionButtons/>
                     {comparisonFeatures.length > 0 &&
@@ -111,10 +78,15 @@ class Sidebar extends React.Component {
             )}  />
           </Switch>
         </div>
-      </PanelContainer>
+      </div>
     );
-  }
 }
+
+Sidebar.propTypes = {
+  select: PropTypes.object.isRequired,
+  selectedFeature: PropTypes.object,
+  comparisonFeatures: PropTypes.array.isRequired
+};
 
 function mapStateToProps(state) {
   return {
