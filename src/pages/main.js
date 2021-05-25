2cef87972e057d38c7a4aed5c5a1861582646ba0
Gerardo Gastaldo <gerardo@toptal.com>
2021-05-25 10:52:44 -0300

    Fix scrolling issues, and branding bar position

--- src/pages/main.js
+++ src/pages/main.js
@@ -22,6 +22,7 @@ const Main = (props) => {
       flexDirection: "row",
       width: "100%",
       height: "100%",
+      overflow: "hidden",
     };
 
     const [comparisonMode, setComparisonMode] = useState(false);
