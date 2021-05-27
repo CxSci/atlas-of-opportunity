fafc31a77bf626d432983451a1fade1cf2be653a
Gerardo Gastaldo <gerardo@toptal.com>
2021-05-27 12:05:50 -0300

    Add "Table / Grid" selector in Header

--- src/components/header.js
+++ src/components/header.js
@@ -3,25 +3,38 @@ import { Link, useLocation } from "react-router-dom";
 
 import { setHeaderOption } from "../redux/action-creators";
 
-import "../css/header.css";
 import { ReactComponent as CloseIcon} from "../assets/close-icon.svg";
+import { ReactComponent as TableIcon} from "../assets/icons/table.svg";
+import { ReactComponent as GridIcon} from "../assets/icons/grid.svg";
+import ButtonGroup from "./ButtonGroup";
+import "../css/header.css";
 
 function Header () {
   const [showDropDown, setShowDropDown] = useState(false)
   const toggleDropDown = useCallback(() => setShowDropDown(state => !state), []);
   const [comparisonMode, setComparisonMode] = useState(false);
   const location = useLocation();
+  const options = [
+    { label: 'Table', value: 'table', icon: <TableIcon /> },
+    { label: 'Grid', value: 'grid', icon: <GridIcon /> },
+  ];
 
   useEffect(() => {
     setComparisonMode(location.pathname.startsWith('/comparison'));
   }, [location]);
+
+  const onBGroupChange = () => {
+
+  }
 
   return (
     <div className={`container ${comparisonMode && "comparisonMode"}`}>
       <div className="navbarLeft">
       </div>
       <div className="navbarCenter">
-        {/* TODO: put conditional comparison controls here */}
+        {comparisonMode && 
+          <ButtonGroup options={options} defaultValue="table" onChange={onBGroupChange} />
+        }
       </div>
       <div className="navbarRight">
         {/* TODO: refactor hamburger menu into its own React component */}
