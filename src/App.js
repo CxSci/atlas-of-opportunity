20b6e6c658cf5658931d5425918ab3cc6a5b1f8a
Justin Anderson <jander@mit.edu>
2021-06-04 00:08:16 -0400

    Show sidebar in comparison view without hack.
    Done by making the sidebar visible either when selectedFeature or comparisonFeatures have content.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from "react";
 import { connect } from "react-redux";
 import PropTypes from 'prop-types';
-
 import { BrowserRouter } from "react-router-dom";
-import Header from "./components/header";
-
-{/* change so App has Main (rather than Routes) */}
-import Main from "./pages/main";
-
 import { Provider } from "react-redux";
+
 import { store } from "./redux/store";
+import Header from "./components/header";
+import Main from "./pages/main";
 
 import "./css/App.css"
 
@@ -17,7 +14,7 @@ const App = (props) => {
   return (
     <BrowserRouter>
       <Provider store={store}>
-        <div className={(props.sidebarOpen && props.selectedFeature)
+        <div className={(props.sidebarOpen && (props.selectedFeature || props.comparisonFeatures))
           ? "sidebarOpen"
           : "sidebarClosed"}>
           <Header/>
@@ -29,12 +26,14 @@ const App = (props) => {
 };
 
 App.propTypes = {
+  comparisonFeatures: PropTypes.arrayOf(PropTypes.object),
   selectedFeature: PropTypes.object,
   sidebarOpen: PropTypes.bool.isRequired,
 };
 
 function mapStateToProps(state) {
   return {
+    comparisonFeatures: state.comparisonFeatures,
     selectedFeature: state.selectedFeature,
     sidebarOpen: state.sidebarOpen,
   };
