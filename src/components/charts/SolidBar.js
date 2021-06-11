4d23d621f7488b56b859b8ee4255e59a76cfdb67
Gerardo Gastaldo <gerardo@toptal.com>
2021-06-11 10:36:33 -0300

    Update SolidBar to use color accoding to the value

--- src/components/charts/SolidBar.js
+++ src/components/charts/SolidBar.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import PropType from 'prop-types';
 import { BarChart, Bar, XAxis, YAxis, LabelList } from 'recharts';
+import { getColorFromGradient } from '../../utils/colors';
 
 const SolidBar = ({ width = 150, height = 24, label, value, max = 100 }) => {
   const data = [{
     name: label,
     value: value,
   }];
+  const fill = getColorFromGradient('rgb(255,233,0)', 'rgb(250,144,4)', value / max);
 
   return (
     <BarChart width={width}
@@ -21,8 +23,8 @@ const SolidBar = ({ width = 150, height = 24, label, value, max = 100 }) => {
       }}>
       <XAxis type="number" domain={[0, max]} hide />
       <YAxis type="category" dataKey="name" hide />
-      <Bar dataKey="value" barSize={height} fill="#ffb266" isAnimationActive={false}>
-        <LabelList dataKey="name" position="insideLeft" />
+      <Bar dataKey="value" barSize={height} fill={fill} isAnimationActive={false}>
+        <LabelList dataKey="name" position="insideLeft" fontSize="12" />
       </Bar>
     </BarChart>
   );
