ee2b859c0998942b4b87de94d9d2274517dfe114
Gerardo Gastaldo <gerardo@toptal.com>
2021-06-17 13:26:52 -0300

    Update LineChartMetric to support series to show names in Legend

--- src/components/charts/LineChartMetric.js
+++ src/components/charts/LineChartMetric.js
@@ -10,48 +10,45 @@ import {
   Tooltip,
 } from "recharts";
 import { getColorFromGradient } from '../../utils/colors';
+const truncate = (str, n) => (str.length > n) ? str.substr(0, n-1) + '…' : str;
 
-const LineChartMetric = ({ width = 260, height = 140, data, showLegend }) => {
+const LineChartMetric = ({ width = 260, height = 140, series, data, showLegend }) => {
   const mgBottom = showLegend ? 0 : -10;
-  if (showLegend) height += 40;
   
   if (data && !Array.isArray(data[0])) {
-    data = [data];
+    series = [{ name: 'Line', data }];
   }
   
-  const lines = data.map((_, idx) => ({
-    dataKey: `value${idx + 1}`,
+  const lines = series.map((item, idx) => ({
+    key: item.name,
+    dataKey: `value`,
+    data: item.data,
+    name: truncate(item.name, 20),
     color: getColorFromGradient('rgb(255,233,36)', 'rgb(242,11,11)', (idx + 1) / 5)
   }));
-  
-  const adapedData = data.reduce((prev, curr, idx) => {
-    if (!prev.length) {
-      prev = curr.map(x => ({ year: x.year }));
-    }
-    prev = prev.map((item, pos) => {
-      const newItem = {...item, [`value${idx + 1}`]: curr[pos].value };
-      return newItem;
-    })
-    return prev;
-  }, []);
+
+  if (showLegend) height += (lines.length * 15);
 
   return (
-    <LineChart width={width} height={height} data={adapedData}
+    <LineChart width={width} height={height}
       margin={{ top: 5, right: 10, left: -2, bottom: mgBottom }}
     >
       <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="year" fontSize={12} />
+      <XAxis dataKey="year" fontSize={12} allowDuplicatedCategory={false} />
       <YAxis type="number" domain={['auto','auto']}
         width={30} fontSize={12}
         tickFormatter={val => val.toLocaleString('en-US', { notation: 'compact', maximumFractionDigits: 0 })} />
       <Tooltip
-        contentStyle={{fontSize: 10, padding: 2}}
-        itemStyle={{padding: '1px 0'}} />
+        contentStyle={{fontSize: 10, padding: '2px 4px'}}
+        labelStyle={{display: 'none'}}
+        itemStyle={{padding: 0}}
+      />
       {showLegend && 
-        <Legend layout="vertical" />
+        <Legend 
+          layout="vertical" />
       }
       {lines.map(line => 
-        <Line key={line.dataKey} dataKey={line.dataKey} stroke={line.color} 
+        <Line key={line.key} dataKey={line.dataKey} data={line.data} name={line.name} stroke={line.color} 
           dot={false} strokeWidth={4} legendType='rect' isAnimationActive={false} />
       )}
     </LineChart>
@@ -63,6 +60,12 @@ LineChartMetric.propTypes = {
   height: PropType.number,
   label: PropType.string,
   data: PropType.array,
+  series: PropType.arrayOf(
+    PropType.shape({
+      name: PropType.string,
+      data: PropType.array,
+    })
+  ),
   showLegend: PropType.bool,
 }
 
