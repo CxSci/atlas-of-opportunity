784e004ab93666ac708f465c68735533909ced30
Justin Anderson <jander@mit.edu>
2021-06-22 13:43:48 -0400

    Set map's max clustering zoom level to 11.

--- src/components/map.js
+++ src/components/map.js
@@ -115,7 +115,7 @@ let Map = class Map extends React.Component {
         // from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
         data: { type: "FeatureCollection", features: this.props.poiFeatures },
         cluster: true,
-        clusterMaxZoom: 14, // Max zoom to cluster points on
+        clusterMaxZoom: 11, // Max zoom to cluster points on
         clusterRadius: 50, // Radius of each cluster when clustering points (defaults to 50)
       });
 
