37234ce485a4804afdd57f6a0dbb5e0d9e3486de
Justin Anderson <jander@mit.edu>
2021-06-25 01:16:58 -0400

    Clear other popups on switch to business layer.

--- src/components/map.js
+++ src/components/map.js
@@ -505,7 +505,13 @@ let Map = class Map extends React.Component {
 
     if (this.props.active.key !== prevProps.active.key) {
       if (this.props.active.key === Constants.MAP_TYPE.BUSINESSES && !this.props.mini) {
-        this.drawBusinessClusters();
+        // Remove popups from non-business layers
+        this.clickedPopup.remove();
+        this.cntr0Popup.remove();
+        this.cntr1Popup.remove();
+        this.cntr2Popup.remove();
+
+        // Remove lines and animated arrows from bridges
         if (this.map.getLayer("route") !== undefined) {
           this.map.removeLayer("route");
           this.map.removeSource("route");
@@ -514,6 +520,8 @@ let Map = class Map extends React.Component {
           this.map.removeLayer("point");
           this.map.removeSource("point");
         }
+
+        this.drawBusinessClusters();
       } else {
         if (this.map.getLayer("clusters")) {
           this.map.removeLayer("clusters");
