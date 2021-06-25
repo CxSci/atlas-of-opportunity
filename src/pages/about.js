73528f523b336e40b0ba6862e1c96981560394d4
Justin Anderson <jander@mit.edu>
2021-06-24 20:30:43 -0400

    Format "About" pages better.
    Use one CSS file to style all of them.

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,17 +1,13 @@
 import React, { Component } from "react";
+
+import ModalContainer from "../components/ModalContainer";
+
 import livingLab from "../assets/livinglab.png";
 import banksa from "../assets/banksa.png";
 import dspark from "../assets/dspark.png";
 
-import ModalContainer from "../components/ModalContainer";
-
 const About = class About extends Component {
   render() {
-    const content = {
-      fontSize: "14px",
-      textAlign: "justify",
-      marginBottom: "15px",
-    };
     const contentImage = {
       marginTop: "25px",
     };
@@ -19,41 +15,41 @@ const About = class About extends Component {
       width: "130px",
       marginRight: "35px",
     };
-    const link = {
-      color: "#f79640",
-    };
     return (
       <ModalContainer title="About the Atlas">
-        <p style={content}>
+        <p>
           The “MIT bigdata Living Lab” (Living Lab) is a research partnership
           enabled by principal partner BankSA, Massachusetts Institute of
           Technology (MIT), the South Australian Government and technical
           partners Optus and DSpark.
         </p>
-        <p style={content}>
+        <p>
+          The Living Lab works to better understand how social interaction
+          and economic behavior impact future outcomes of communities across
+          South Australia. This map showcases recent research revealing that
+          understanding community movement patterns is crucial for
+          understanding economic growth and mobility. Places with more
+          diverse movement patterns are more likely to have higher
+          near-future economic growth. The goal of the Atlas is to make
+          these insights more accessible.
+        </p>
+        <p>
           Located at Adelaide’s Lot Fourteen’s innovation precinct, the Living
           Lab works closely with local universities, the business community and
           the public sector to safely and securely analyse and present big data.
           It is an example of business, universities and government
           collaborating to help grow the South Australian economy.
         </p>
-        <p style={content}>
+        <p>
           MIT shares its world leading best practice data analytics methodology
           to the research undertaken through the Living Lab. It follows a
           similar model to those already established in New York, Beijing and
           Istanbul.
         </p>
-        <div style={content}>
-          Further information on the MIT Big Data Living Lab can be found on
-          <br />
-          <a
-            href="https://dti.sa.gov.au/industry/big-data-living-lab" target="_blank" rel="noopener noreferrer"
-            style={link}
-          >
-            https://dti.sa.gov.au/industry/big-data-living-lab
-          </a>
-        </div>
-        <p style={content}>
+        <p>
+          Further information on the MIT Big Data Living Lab can be found at <a href="https://dti.sa.gov.au/industry/big-data-living-lab" target="_blank" rel="noopener noreferrer" >https://dti.sa.gov.au/industry/big-data-living-lab</a>
+        </p>
+        <p>
           For questions and comments about the Atlas of Opportunity, please email <a href="mailto:opportunity@mit.edu">opportunity@mit.edu</a>.
         </p>
         <div style={contentImage}>
