7ababc6618e14164dba1c9311436f5231dd65635
Gerardo Gastaldo <gerardo@toptal.com>
2021-06-30 11:53:51 -0300

    Remove hardcoding for colors, min, max

--- src/components/legend.js
+++ src/components/legend.js
@@ -10,11 +10,58 @@ import "../css/legend.css";
 
 function Legend({ activeLayer, mapLayers, absolute = false }) {
   const { name, description, stops } = activeLayer;
+  const min = stops[0][0];
+  const max = stops[2][0];
+  const colors = stops.map(x => x[1]);
 
   const mapTypeEvent = (value) => {
     const mapType = Object.keys(mapLayers).find(t => mapLayers[t].name === value)
     setMapType(mapType);
   };
+
+  const renderGradientBar = ({ width, height }) => {
+    const data = [
+      { name: "bar", value: max },  // the Bar itself
+      { name: "empty", value: 0 },  // empty bar for ReferenceArea
+    ];
+    height = height * 2;
+  
+    return (
+      <BarChart width={width}
+        height={height}
+        data={data}
+        layout="vertical"
+        margin={{ top: 0, left: 0, bottom: 0, right: 0, }}
+      >
+        <XAxis type="number" domain={[min, max]} hide />
+        <YAxis type="category" dataKey="name" hide />
+        <Bar dataKey="value" barSize={height} shape={renderShape} isAnimationActive={false} />
+        <ReferenceArea x1={0} x2={max} y1={"empty"} fill="transparent">
+          <Label value={min} position="insideLeft" fontSize={14} />
+          <Label value={max / 2} position="center" fontSize={14} />
+          <Label value={max} position="insideRight" fontSize={14} />
+        </ReferenceArea>
+      </BarChart>
+    );
+  }
+  
+  const renderShape = ({ height, width, x, y }) => {
+    const lineX = width / 2;
+  
+    return (
+      <svg x={x} y={y}>
+        <defs>
+          <linearGradient id={`legend-gradient`} x1="0" y1="0" x2="1" y2="0">
+            <stop offset="0%" style={{ stopColor: colors[0] }} />
+            <stop offset="50%" style={{ stopColor: colors[1] }} />
+            <stop offset="100%" style={{ stopColor: colors[2] }} />
+          </linearGradient>
+        </defs>
+        <rect width={width} height={height} fill={`url(#legend-gradient)`} />
+        <line x1={lineX} y1={0} x2={lineX} y2={height} stroke="white" strokeDasharray="1 1"/>
+      </svg>
+    );
+  }
 
   return (
     <div className={`legend`} style={{position: absolute ? "absolute" : undefined}}>
@@ -28,54 +75,9 @@ function Legend({ activeLayer, mapLayers, absolute = false }) {
       {description && <div className="mb6">
         <p className="txt-s color-gray">{description}{JSON.stringify(stops)}</p>
       </div>}
-      {renderGradientBar({width: 276, height: 30, max: 1.2})}
+      {renderGradientBar({width: 276, height: 30})}
     </div>
   )
-}
-
-const renderGradientBar = ({ width, height, min = 0, max = 1 }) => {
-  const data = [
-    { name: "bar", value: max },  // the Bar itself
-    { name: "empty", value: 0 },  // empty bar for ReferenceArea
-  ];
-  height = height * 2;
-
-  return (
-    <BarChart width={width}
-      height={height}
-      data={data}
-      layout="vertical"
-      margin={{ top: 0, left: 0, bottom: 0, right: 0, }}
-    >
-      <XAxis type="number" domain={[0, max]} hide />
-      <YAxis type="category" dataKey="name" hide />
-      <Bar dataKey="value" barSize={height} shape={renderShape} isAnimationActive={false} />
-      <ReferenceArea x1={0} x2={max} y1={"empty"} fill="transparent">
-        <Label value={min} position="insideLeft" fontSize={14} />
-        <Label value={max / 2} position="center" fontSize={14} />
-        <Label value={max} position="insideRight" fontSize={14} />
-      </ReferenceArea>
-    </BarChart>
-  );
-}
-
-const renderShape = ({ height, width, x, y }) => {
-  const minColor = 'rgb(255,233,0)';
-  const maxColor = 'rgb(242,11,11)';
-  const lineX = width / 2;
-
-  return (
-    <svg x={x} y={y}>
-      <defs>
-        <linearGradient id={`legend-gradient`} x1="0" y1="0" x2="1" y2="0">
-          <stop offset="0%" style={{ stopColor: minColor }} />
-          <stop offset="100%" style={{ stopColor: maxColor }} />
-        </linearGradient>
-      </defs>
-      <rect width={width} height={height} fill={`url(#legend-gradient)`} />
-      <line x1={lineX} y1={0} x2={lineX} y2={height} stroke="white" strokeDasharray="1 1"/>
-    </svg>
-  );
 }
 
 Legend.propTypes = {
