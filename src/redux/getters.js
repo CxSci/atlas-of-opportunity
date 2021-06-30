ad9ba9f9aaac2aa8aa1c349530f7f24fcd74db83
Justin Anderson <jander@mit.edu>
2021-06-29 21:05:16 -0400

    Add ANZSIC codes to redux.

--- src/redux/getters.js
+++ src/redux/getters.js
@@ -3,6 +3,7 @@ export const getComparisonFeatures = (state) => state.comparisonFeatures;
 export const getComparisonType = (state) => state.comparisonType;
 export const getHamburgerMenuOpen = (state) => state.hamburgerMenuOpen;
 export const getHiddenSidebarDialogs = (state) => state.hiddenSidebarDialogs;
+export const getANZSICCodes = (state) => state.anzsicCodes;
 
 // Two features are considered equal is their id numbers are the same.
 // Necessary to prevent hooks from thinking two features are different just
