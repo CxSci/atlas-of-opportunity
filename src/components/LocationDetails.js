72d94d4a963b4d1ae1d9fb60ab4da244f69e78a0
ENIGMA3THRUST <46653672+ENIGMA3THRUST@users.noreply.github.com>
2021-10-28 05:02:01 +0800

    Added virtualized module
    Using the react-viewport-list module to archive list virtualizing. Also multiply the number of the collapsible sections.

--- src/components/LocationDetails.js
+++ src/components/LocationDetails.js
@@ -13,18 +13,33 @@ import SolidBar from "./charts/SolidBar";
 import LineChartMetric from "./charts/LineChartMetric";
 import generateMetrics from "../utils/generateMetrics";
 
+import ViewportList from '../../node_modules/react-viewport-list';
+
+
+import {useRef } from 'react';
+
+
+
+
 const LocationDetails = (props) => {
   const selectedFeature = props.feature;
   const comparisonFts = props.comparison;
   const anzsicCodes = useSelector(getANZSICCodes);
+
+  
+
+
 
   let allFeatures = comparisonFts;
   if (selectedFeature && !comparisonFts.find(feature => feature.properties["SA2_MAIN16"] === selectedFeature.properties["SA2_MAIN16"])) {
     allFeatures = [...allFeatures, props.feature]
   }
 
+
   const renderMetric = (metric) => {
     const label = formatLabel(metric, anzsicCodes)
+
+
     return (
       <div key={metric.id} className="metric">
         <h2 data-tip data-for={metric.id}>{label}</h2>
@@ -65,23 +80,85 @@ const LocationDetails = (props) => {
     }
   }
 
+
+
+
+ // Bulid the virtualizing list 
+  const ItemsList = ({ items }) => {
+    const ref = useRef(null);
+
+    return (
+        <div className="scroll-container" ref={ref}>
+            <ViewportList
+                viewportRef={ref}
+                items={items}
+                itemMinSize={40}
+                margin={8}
+            >
+                {(item) => (
+                  
+                    <div key={item.key} className="item">
+                        {item}
+                    </div>
+                )}
+            </ViewportList>
+        </div>
+    );
+};
+
+ItemsList.propTypes = {
+  items: PropTypes.any.isRequired
+}
+
+// store all the collapsible sections in the list for further virtualizing 
+var Data = [];
+
+
+
+
+
+
   return (
+    
     <>
+    
+      
       {props.children}
       {propsMapping.map((section) => {
         {/* Preprocess section.content to expand its generators */}
         const metrics = generateMetrics(section.content, allFeatures);
-        
-        return (
-          <CollapsibleSection title={section.title} key={section.title}>
+
+        for (var i=0;i<20;i++){
+          const collapsible = (<CollapsibleSection title={section.title + i.toString()} key={section.title + i.toString()}>
             {metrics.length === 0 &&
               <div className="metric" style={{padding: '10px 0'}}>No Data</div>
             }
             {metrics.map(renderMetric)}
-          </CollapsibleSection>
-        )}
+          </CollapsibleSection>);
+          Data.push(collapsible);
+
+        }
+
+
+
+
+
+      }
       )}
+      
+
+      <ItemsList items={Data} />
+
+      
+
+
+      
+
+    
+
     </>
+
+
   )
 }
 
