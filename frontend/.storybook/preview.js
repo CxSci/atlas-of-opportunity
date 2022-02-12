8293d7d447eb51a65b42dd7e93d5e104714d98c2
Eric Torosyan <torosyaneric@gmail.com>
2022-02-12 10:19:09 +0400

    Added 404 Page + redirect, Fixed Storybook related issues: path trailing slash, router error

--- frontend/.storybook/preview.js
+++ frontend/.storybook/preview.js
@@ -1,4 +1,8 @@
 import 'styles/index.scss'
+import { addDecorator } from '@storybook/react'
+import { MemoryRouter } from 'react-router'
+
+addDecorator(story => <MemoryRouter initialEntries={['/']}>{story()}</MemoryRouter>)
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,6 +1,6 @@
 // vendors
 import React from 'react'
-import { Route, Routes as Switch } from 'react-router-dom'
+import { Navigate, Route, Routes as Switch } from 'react-router-dom'
 
 // utils
 import PATH from '../../../frontend/src/utils/path'
@@ -14,6 +14,7 @@ import GuidedTool from './GuidedTool'
 import Contributors from './Contributors'
 import Research from './Research'
 import Faq from './Faq'
+import NotFound from './404'
 
 function Routes() {
   return (
@@ -33,6 +34,9 @@ function Routes() {
       <Route exact path={PATH.FAQ} element={<Faq />} />
       <Route exact path={PATH.RESEARCH} element={<Research />} />
       <Route exact path={PATH.CONTRIBUTORS} element={<Contributors />} />
+
+      <Route exact path={PATH[404]} element={<NotFound />} />
+      <Route path="*" element={<Navigate to={PATH[404]} />} />
     </Switch>
   )
 }
