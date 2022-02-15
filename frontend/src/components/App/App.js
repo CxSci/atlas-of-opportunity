bcbbaf791df21bdc00042c31e7462a0ee473987b
Eric Torosyan <torosyaneric@gmail.com>
2022-02-15 23:51:43 +0400

    Add Sidebar, update theme, add dark mode, update app component

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useMemo, useState } from 'react'
 import Routes from '../../routes'
+import Header from '../Header'
+import Sidebar from '../Sidebar/Sidebar'
+import AppContainer from './AppContainer'
+import { ThemeProvider } from '@mui/material'
+import initTheme from '../../utils/theme'
+import CssBaseline from '@mui/material/CssBaseline'
 
 function App() {
+  // TODO: move to Redux store
+  const [sidebarOpen, setSidebarOpen] = useState(false)
+  const [darkTheme, setDarkTheme] = useState(false)
+
+  const theme = useMemo(() => initTheme(darkTheme), [darkTheme])
+
   return (
-    <>
-      <Routes />
-    </>
+    <ThemeProvider theme={theme}>
+      <AppContainer>
+        <Header toggleSidebar={open => setSidebarOpen(open)} />
+        <Sidebar open={sidebarOpen} handleClose={() => setSidebarOpen(false)} />
+
+        <Routes />
+      </AppContainer>
+
+      <CssBaseline />
+    </ThemeProvider>
   )
 }
 
--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,7 +1,25 @@
 import React from 'react'
+import { Box, IconButton } from '@mui/material'
+import { Menu as MenuIcon } from '@mui/icons-material'
 
-function Header() {
-  return <div></div>
+function Header({ toggleSidebar }) {
+  return (
+    <Box
+      sx={{
+        height: theme => theme.components.header.height,
+        paddingX: theme => theme.components.header.paddingX,
+        paddingY: theme => theme.components.header.paddingY,
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        right: 0,
+        backgroundColor: theme => theme.palette.background
+      }}>
+      <IconButton aria-label="delete" onClick={toggleSidebar}>
+        <MenuIcon />
+      </IconButton>
+    </Box>
+  )
 }
 
 export default Header
