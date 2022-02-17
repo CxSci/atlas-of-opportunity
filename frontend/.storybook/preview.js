20b1a0dcd663ceb40e0fa3da4abb644a011f1e16
Leo Esaki <leo.esaki3@gmail.com>
2022-02-18 03:22:39 +0900

    Wrap storybook components with providers in preview.js

--- frontend/.storybook/preview.js
+++ frontend/.storybook/preview.js
@@ -1,6 +1,10 @@
 import 'styles/index.scss'
 import { addDecorator } from '@storybook/react'
 import { MemoryRouter } from 'react-router'
+import { IntlProvider } from 'react-intl'
+import { ThemeProvider } from '@mui/material'
+
+import initTheme from '../src/utils/theme'
 
 addDecorator(story => <MemoryRouter initialEntries={['/']}>{story()}</MemoryRouter>)
 
@@ -13,3 +17,16 @@ export const parameters = {
     }
   }
 }
+
+const withProviders = (Story, context) => {
+  const theme = initTheme(false)
+  return (
+    <IntlProvider locale={navigator.language}>
+      <ThemeProvider theme={theme}>
+        <Story {...context} />
+      </ThemeProvider>
+    </IntlProvider>
+  )
+}
+
+export const decorators = [withProviders]
