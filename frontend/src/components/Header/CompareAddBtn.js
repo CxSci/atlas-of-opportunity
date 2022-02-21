28c798d8f82bb8a6fa59b991025c3452784332ba
Eric Torosyan <torosyaneric@gmail.com>
2022-02-21 11:04:43 +0400

    Updated compare btns text

--- frontend/src/components/Header/CompareAddBtn.js
+++ frontend/src/components/Header/CompareAddBtn.js
@@ -1,15 +1,15 @@
 import React from 'react'
+import { Button, Typography } from '@mui/material'
 import CompareIconPlus from '../Icons/CompareIconPlus'
-import { Box, Button } from '@mui/material'
 
 function CompareAddBtn(props) {
   return (
     <Button variant="contained" color={'primary'} sx={{ color: '#fff', ml: 2 }} {...props}>
       <CompareIconPlus />
 
-      <Box component={'span'} ml={1}>
+      <Typography component={'span'} sx={{ ml: 1 }}>
         Add to comparison
-      </Box>
+      </Typography>
     </Button>
   )
 }
