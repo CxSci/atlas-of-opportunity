cdc72f7a5a1d8381ed14b168c905bdf52cbed65a
Eric Torosyan <torosyaneric@gmail.com>
2022-02-21 11:02:00 +0400

    Change header search to autocomplete

--- frontend/src/components/Header/SearchInput.js
+++ frontend/src/components/Header/SearchInput.js
@@ -1,22 +1,37 @@
 import React from 'react'
-import { FormControl, Input, InputAdornment } from '@mui/material'
-import { Search as SearchIcon } from '@mui/icons-material'
+import { Autocomplete, InputAdornment, TextField } from '@mui/material'
+import { Close as CloseIcon, Search as SearchIcon } from '@mui/icons-material'
 
 function SearchInput({ placeholder }) {
+  const options = [
+    { label: 'The Godfather', id: 1 },
+    { label: 'Pulp Fiction', id: 2 },
+  ]
+
   return (
-    <FormControl variant="filled">
-      <Input
-        placeholder={placeholder}
-        // TODO: if border has to be removed
-        // sx={{ ':before': { content: 'none' }, ':after': { content: 'none' } }}
-        sx={{ width: theme => theme.components.searchInput.width }}
-        startAdornment={
-          <InputAdornment position="start" sx={{ mt: '0 !important' }}>
-            <SearchIcon />
-          </InputAdornment>
-        }
-      />
-    </FormControl>
+    <Autocomplete
+      freeSolo
+      id="search-input-autocomplete"
+      clearIcon={<CloseIcon />}
+      options={options}
+      getOptionLabel={item => item?.label}
+      renderInput={params => (
+        <TextField
+          {...params}
+          sx={{ width: theme => theme.components.searchInput.width }}
+          variant={'filled'}
+          InputProps={{
+            ...params.InputProps,
+            placeholder,
+            startAdornment: (
+              <InputAdornment position="start" sx={{ mt: '0 !important' }}>
+                <SearchIcon />
+              </InputAdornment>
+            ),
+          }}
+        />
+      )}
+    />
   )
 }
 
