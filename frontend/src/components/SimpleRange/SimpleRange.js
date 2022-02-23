4cdfbaac7596ce98597efc318db2e5b197cdf584
Leo Esaki <leo.esaki3@gmail.com>
2022-02-23 09:26:18 +0900

    Fix commented issues

--- frontend/src/components/SimpleRange/SimpleRange.js
+++ frontend/src/components/SimpleRange/SimpleRange.js
@@ -1,18 +1,18 @@
-import { FormattedNumber } from 'react-intl'
 import { styled } from '@mui/material/styles'
 import LinearProgress, { linearProgressClasses } from '@mui/material/LinearProgress'
-import Tooltip from '@mui/material/Tooltip'
 import PropTypes from 'prop-types'
 
 const GradientProgress = styled(LinearProgress)(({ theme, value }) => ({
   [`& .${linearProgressClasses.bar}`]: {
     overflow: 'hidden',
     backgroundColor: 'transparent',
+    borderRadius: 1,
     '&:before': {
       content: '""',
       display: 'block',
       width: '100%',
       height: '100%',
+      borderRadius: 1,
       transition: 'transform .4s linear',
       transform: `translateX(${100 - value}%)`,
       background: `linear-gradient(270deg, ${theme.palette.canary.main} 0%, ${theme.palette.chestnutRose.main} 50%, ${theme.palette.ultramarine.main} 100%)`, // eslint-disable-line
@@ -24,9 +24,12 @@ const SimpleRange = ({ min, max, value, style, color }) => {
   const Range = style === 'gradient' ? GradientProgress : LinearProgress
   const percentage = Math.min(1, (value - min) / (max - min || 1))
   return (
-    <Tooltip title={<FormattedNumber value={value} />} placement="top">
-      <Range value={percentage * 100} sx={{ height: 24, borderRadius: 1 }} variant="determinate" color={color} />
-    </Tooltip>
+    <Range
+      value={percentage * 100}
+      sx={{ height: 24, borderRadius: 1, [`& .${linearProgressClasses.bar}`]: { borderRadius: 1 } }}
+      variant="determinate"
+      color={color}
+    />
   )
 }
 
