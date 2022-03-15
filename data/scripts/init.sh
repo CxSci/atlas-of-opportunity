2644997fb2d6bd3c43f4a2607ebe60bf3dd63bd2
Justin Anderson <jander@mit.edu>
2022-03-15 13:47:33 -0400

    Move docker-entrypoint-initdb.d to subdirectories
    - Add recursive search for entrypoint scripts
    
    - Make load order more explicit with numeric prefixes
    
    - Load regional data via docker-compose volume instead of baking into image

---
+++ data/scripts/init.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Execute docker entrypoint scripts in subdirectories
+
+find /docker-entrypoint-initdb.d -mindepth 2 -type f -print0 | while read -d $'\0' f; do
+  case "$f" in
+    *.sh)
+      if [ -x "$f" ]; then
+        echo "$0: running $f"
+        "$f"
+      else
+        echo "$0: sourcing $f"
+        . "$f"
+      fi
+      ;;
+    *.sql)    echo "$0: running $f"; "${psql[@]}" -f "$f"; echo ;;
+    *.sql.gz) echo "$0: running $f"; gunzip -c "$f" | "${psql[@]}"; echo ;;
+    *)        echo "$0: ignoring $f" ;;
+  esac
+  echo
+done
