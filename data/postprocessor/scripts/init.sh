fcf8c0da956db091365289e67b7bc354871eeef5
Justin Anderson <jander@mit.edu>
2022-03-22 00:49:46 -0400

    Add an ephemeral postprocessor service
    - To be used for scripts which need to run after the PostGIS server is up
    
    - Quits early if it already ran successfully
    
    - Uses a sentinel table in postgis named 'dashboard_postprocessor' to track whether it ever ran successfully

---
+++ data/postprocessor/scripts/init.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# Wait for postgis server to be available
+python3 /scripts/wait_for_postgresql.py "select true"
+
+SENTINEL="dashboard_postprocessor"
+CHECK_SQL="SELECT to_regclass('public.${SENTINEL}');"
+MARK_FINISHED_SQL="CREATE TABLE ${SENTINEL}();"
+
+if [[ $(psql -A -t -c "$CHECK_SQL") != "$SENTINEL" ]]; then
+  echo "Sentinel table '${SENTINEL}' does not exist."
+  echo "Running postprocessor entrypoint scripts..."
+  # Execute docker entrypoint scripts
+  #
+  # - Execute .sh and .py files
+  # - Look in subdirectories
+  find /datasets/*/postprocessor-entrypoint-scripts -type f -print0 | while read -d $'\0' f; do
+    case "$f" in
+      *.sh)    echo "$0: running $f"; sh "$f" ;;
+      *.py)    echo "$0: running $f"; python3 "$f"; echo ;;
+      *)       echo "$0: ignoring $f" ;;
+    esac
+    echo
+  done
+
+  echo "Finished running entrypoint scripts. Creating sentinel table to prevent future runs."
+  psql -c "$MARK_FINISHED_SQL"
+else
+  echo "Sentinel table '${SENTINEL}' already exists."
+  echo "Postprocessor exiting early."
+fi
+
+echo "Postprocessor finished. Shutting down."
