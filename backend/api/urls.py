1008dd976030784e6fd5220fcae2d032a52be955
Justin Anderson <jander@mit.edu>
2022-03-24 23:25:08 -0400

    Apply linter suggestions to backend

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import include, path
 from rest_framework import routers
+
 from api import views
 
 router = routers.DefaultRouter()
@@ -12,12 +13,12 @@ urlpatterns = [
     path(
         "datasets/<slug:dataset>/metrics/<slug:metric>/",
         views.ExploreMetricView.as_view(),
-        name="explore-metric-detail"
+        name="explore-metric-detail",
     ),
     path(
         "datasets/<slug:dataset>/detail/<str:pk>/",
         views.DetailView.as_view(),
-        name="dataset-detail"
+        name="dataset-detail",
     ),
     path(
         "api-auth/", include("rest_framework.urls", namespace="rest_framework")
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('', include('api.urls')),
+    path("", include("api.urls")),
 ]
