2202e785aadb0aca49e430c1db58625cf680543f
Leo Esaki <leo.esaki3@gmail.com>
2022-03-28 22:26:05 -0300

    Delete unexpected files

--- frontend/src/store/modules/dataSet/actions.js
+++
@@ -1,6 +0,0 @@
-import { createAction } from 'redux-actions'
-import * as types from './types'
-
-export const getDatasetList = createAction(types.GET_DATASET_LIST)
-export const getDatasetSingle = createAction(types.GET_DATASET_SINGLE)
-export const getDatasetDetailData = createAction(types.GET_DATASET_DETAIL_DATA)
--- frontend/src/store/modules/dataSet/index.js
+++
@@ -1,8 +0,0 @@
-import reducer from './reducers'
-
-export * from './actions'
-export * from './selectors'
-export * from './types'
-export { default as saga } from './sagas'
-
-export default reducer
