353c604112958c3c5c7f3fbcf123a208d90bda8f
Justin Anderson <jander@mit.edu>
2022-03-30 01:35:08 -0400

    Make sure new db indexes exist after postprocessor

--- data/datasets/south_australia/postprocessor-entrypoint-scripts/40_create_indexes.sql
+++ data/datasets/south_australia/postprocessor-entrypoint-scripts/40_create_indexes.sql
@@ -8,3 +8,5 @@ CREATE INDEX IF NOT EXISTS sa2_2016_aust_sa2_name16_idx
     ON public.sa2_2016_aust USING btree
     (sa2_name16 COLLATE pg_catalog."default" ASC NULLS LAST)
     TABLESPACE pg_default;
+
+ANALYZE sa2_2016_aust;
