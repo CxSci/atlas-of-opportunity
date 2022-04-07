43db7f530dacb4ebf34bc3d746551764bb96a824
Leo Esaki <leo.esaki3@gmail.com>
2022-04-07 12:53:31 -0300

    Fix comments

--- frontend/src/components/StaticMap/StaticMap.utils.js
+++ frontend/src/components/StaticMap/StaticMap.utils.js
@@ -8,11 +8,11 @@ export const generateSize = (ref, square, mapHeight) => {
   }
 }
 
-export const scaleRatio = 0.55
+export const scaleRatio = 0.55 // Default is 1, but we need this value to show proper size of the requested map
 
 export const getScaleTranslation = (pathGenerator, location, width, height) => {
-  const b = pathGenerator.bounds(location)
-  const s = scaleRatio / Math.max((b[1][0] - b[0][0]) / width, (b[1][1] - b[0][1]) / height)
-  const t = [(width - s * (b[1][0] + b[0][0])) / 2, (height - s * (b[1][1] + b[0][1])) / 2]
+  const b = pathGenerator.bounds(location) //  Get boundary coordinate of a map
+  const s = scaleRatio / Math.max((b[1][0] - b[0][0]) / width, (b[1][1] - b[0][1]) / height) //  amplification number of original map
+  const t = [(width - s * (b[1][0] + b[0][0])) / 2, (height - s * (b[1][1] + b[0][1])) / 2] //  coordinate to focus on the requested map
   return [s, t]
 }
