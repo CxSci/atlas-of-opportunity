ea10d47048d669e425b68bda237d6e93bf1adb76
Leo Esaki <leo.esaki3@gmail.com>
2022-04-28 13:41:48 -0300

    Set minimal width for simple bar

--- frontend/src/components/SimpleBar/SimpleBar.js
+++ frontend/src/components/SimpleBar/SimpleBar.js
@@ -1,3 +1,4 @@
+import { useCallback, useMemo, useState } from 'react'
 import Box from '@mui/material/Box'
 import Grid from '@mui/material/Grid'
 import LinearProgress, { linearProgressClasses } from '@mui/material/LinearProgress'
@@ -7,26 +8,40 @@ import { NumberFormatType } from 'utils/propTypes'
 import FieldNumber from 'components/FieldNumber'
 
 const SimpleBar = ({ value, percentage, numberFormat }) => {
+  const [node, setNode] = useState(null)
+  const getNode = useCallback(
+    node => {
+      if (node) {
+        setNode(node)
+      }
+    },
+    [setNode],
+  )
+  const fullWidth = node?.clientWidth
+  const width = useMemo(() => Math.max(fullWidth * percentage, 8), [fullWidth, percentage])
+
   return (
-    <Grid container spacing={1} alignItems="center" flexWrap="nowrap">
-      <Grid item flex={1}>
-        <Box sx={{ position: 'relative', width: `${(percentage || value) * 100}%` }}>
-          <LinearProgress
-            value={100}
-            sx={{ height: 24, borderRadius: 1, [`& .${linearProgressClasses.bar}`]: { borderRadius: 1 } }}
-            variant="determinate"
-            color="secondary"
-          />
-          <Box sx={{ position: 'absolute', left: '100%', top: '50%', ml: 1, transform: 'translateY(-50%)' }}>
-            <FieldNumber value={value} numberFormat={numberFormat} />
+    <Box ref={getNode}>
+      <Grid container spacing={1} alignItems="center" flexWrap="nowrap">
+        <Grid item flex={1}>
+          <Box sx={{ position: 'relative', width }}>
+            <LinearProgress
+              value={100}
+              sx={{ height: 24, borderRadius: 1, [`& .${linearProgressClasses.bar}`]: { borderRadius: 1 } }}
+              variant="determinate"
+              color="secondary"
+            />
+            <Box sx={{ position: 'absolute', left: '100%', top: '50%', ml: 1, transform: 'translateY(-50%)' }}>
+              <FieldNumber value={value} numberFormat={numberFormat} />
+            </Box>
           </Box>
-        </Box>
+        </Grid>
+        <Grid item sx={{ visibility: 'hidden' }}>
+          {/* render again to make it take up space for the number rendered above*/}
+          <FieldNumber value={value} numberFormat={numberFormat} />
+        </Grid>
       </Grid>
-      <Grid item sx={{ visibility: 'hidden' }}>
-        {/* render again to make it take up space for the number rendered above*/}
-        <FieldNumber value={value} numberFormat={numberFormat} />
-      </Grid>
-    </Grid>
+    </Box>
   )
 }
 
