62e02ad30f39f5a2c27d8c1ca626b7bfd96879fc
Leo Esaki <leo.esaki3@gmail.com>
2022-05-03 11:31:38 -0300

    Revert original commit and implement suggested feature

--- frontend/src/components/SimpleBar/SimpleBar.js
+++ frontend/src/components/SimpleBar/SimpleBar.js
@@ -1,4 +1,3 @@
-import { useEffect, useCallback, useMemo, useState, useRef } from 'react'
 import Box from '@mui/material/Box'
 import Grid from '@mui/material/Grid'
 import LinearProgress, { linearProgressClasses } from '@mui/material/LinearProgress'
@@ -8,50 +7,18 @@ import { NumberFormatType } from 'utils/propTypes'
 import FieldNumber from 'components/FieldNumber'
 
 const SimpleBar = ({ value, percentage, numberFormat }) => {
-  const containerRef = useRef(null)
-  const fieldRef = useRef(null)
-  const [fullWidth, setFullWidth] = useState(containerRef.current?.clientWidth)
-  const fieldWidth = fieldRef.current?.clientWidth
-  const handleContainerResize = useCallback(() => setFullWidth(containerRef.current?.clientWidth), [setFullWidth])
-  const getContainerNode = useCallback(
-    node => {
-      if (node) {
-        containerRef.current = node
-        handleContainerResize()
-      }
-    },
-    [handleContainerResize],
-  )
-  const getFieldNode = useCallback(node => {
-    if (node) {
-      fieldRef.current = node
-    }
-  }, [])
-
-  useEffect(() => {
-    window.addEventListener('resize', handleContainerResize)
-    return () => window.removeEventListener('resize', handleContainerResize)
-  }, [handleContainerResize])
-
-  const width = useMemo(
-    () => Math.max((fullWidth - fieldWidth - 8) * percentage, 8),
-    [fullWidth, fieldWidth, percentage],
-  )
-
   return (
-    <Box ref={getContainerNode}>
+    <Box>
       <Grid container spacing={1} alignItems="center" flexWrap="nowrap">
         <Grid item flex={1}>
-          <Box sx={{ position: 'relative', width }}>
+          <Box sx={{ position: 'relative', width: `${percentage * 100}%`, minWidth: '8px' }}>
             <LinearProgress
               value={100}
               sx={{ height: 24, borderRadius: 1, [`& .${linearProgressClasses.bar}`]: { borderRadius: 1 } }}
               variant="determinate"
               color="secondary"
             />
-            <Box
-              ref={getFieldNode}
-              sx={{ position: 'absolute', left: '100%', top: '50%', ml: 1, transform: 'translateY(-50%)' }}>
+            <Box sx={{ position: 'absolute', left: '100%', top: '50%', ml: 1, transform: 'translateY(-50%)' }}>
               <FieldNumber value={value} numberFormat={numberFormat} />
             </Box>
           </Box>
