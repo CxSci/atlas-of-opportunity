729e8e6f0e45327136c8d51eeea47d2fe2a45343
Eric Torosyan <torosyaneric@gmail.com>
2022-05-04 17:08:50 +0400

    Truncate header breadcrumbs on smaller screens: rightmost items should be truncated first

--- frontend/src/components/AtlasBreadcrumbs/AtlasBreadcrumbs.js
+++ frontend/src/components/AtlasBreadcrumbs/AtlasBreadcrumbs.js
@@ -1,7 +1,10 @@
-import React from 'react'
-import { Breadcrumbs, Link } from '@mui/material'
+import React, { useEffect, useLayoutEffect, useRef, useState } from 'react'
+import { Breadcrumbs } from '@mui/material'
 import PATH from '../../utils/path'
 import PropTypes from 'prop-types'
+import BreadcrumbLink from './BreadcrumbLink'
+import { useSelector } from 'react-redux'
+import { isRequestPending } from '../../store/modules/api'
 
 export const homeBreadcrumbLink = {
   path: PATH.HOME,
@@ -9,12 +12,79 @@ export const homeBreadcrumbLink = {
 }
 
 function AtlasBreadcrumbs({ links = [] }) {
+  const containerRef = useRef()
+  const cloneRef = useRef()
+  const [containerWidth, setContainerWidth] = useState(0)
+  const [widthsList, setWidthsList] = useState([])
+  const [truncateCount, setTruncateCount] = useState(0)
+  const isEntryLoading = useSelector(isRequestPending('datasetDetailData', 'get'))
+  const isDatasetLoading = useSelector(isRequestPending('datasetList', 'get'))
+
+  useLayoutEffect(() => {
+    if (cloneRef.current) {
+      document.body.removeChild(cloneRef.current)
+    }
+
+    const containerNodeClone = containerRef?.current?.cloneNode?.(true)
+    containerNodeClone.style = ' opacity: 0; max-height: 0'
+    containerNodeClone.childNodes[0].style = 'display: inline-flex'
+    containerNodeClone.querySelectorAll('.header__link--truncated').forEach(link => {
+      link.classList.remove('header__link--truncated')
+    })
+    cloneRef.current = containerNodeClone
+    document.body.appendChild(containerNodeClone)
+
+    const containerWidth = containerNodeClone?.childNodes?.[0]?.offsetWidth
+    setContainerWidth(containerWidth)
+    const childNodes = containerNodeClone?.querySelectorAll?.('li:not(.MuiBreadcrumbs-separator)') || []
+    const widthsList = []
+    ;[...childNodes].forEach((node, index) => widthsList.push(node.offsetWidth))
+
+    setWidthsList(widthsList)
+  }, [isEntryLoading, isDatasetLoading])
+
+  useEffect(() => {
+    function handleResize() {
+      let breadcrumbsWidth = containerWidth
+      const windowWidth = window.innerWidth
+      const headerRightContentNode = document.querySelector('.header__right-content')
+      const headerActionBtnNode = document.querySelector('.header__action-btn')
+      const rightContentWidth = headerRightContentNode?.offsetWidth
+      const actionBtnWidth = headerActionBtnNode?.offsetWidth + 16
+
+      const allocatedBreadcrumbsWidth = windowWidth - rightContentWidth - actionBtnWidth - 40
+
+      let truncateCount = 0
+      links.forEach((item, index) => {
+        if (breadcrumbsWidth > allocatedBreadcrumbsWidth) {
+          const itemWidth = widthsList[links.length - 1 - index]
+          const ellipsisWidth = 22
+          breadcrumbsWidth -= itemWidth - ellipsisWidth
+          truncateCount++
+        }
+      })
+
+      setTruncateCount(truncateCount)
+    }
+
+    handleResize()
+    window.addEventListener('resize', handleResize)
+
+    return () => window.removeEventListener('resize', handleResize)
+  }, [containerWidth, links, widthsList])
+
+  useEffect(() => {
+    return () => {
+      if (cloneRef.current) {
+        document.body.removeChild(cloneRef.current)
+      }
+    }
+  }, [])
+
   return (
-    <Breadcrumbs aria-label="breadcrumb">
+    <Breadcrumbs ref={containerRef} aria-label="breadcrumb">
       {(links || []).map((linkItem, index) => (
-        <Link key={index} underline={linkItem?.path ? 'hover' : 'none'} color="inherit" href={linkItem?.path}>
-          {linkItem?.text}
-        </Link>
+        <BreadcrumbLink key={index} linkItem={linkItem} truncate={index >= links.length - truncateCount} />
       ))}
     </Breadcrumbs>
   )
---
+++ frontend/src/components/AtlasBreadcrumbs/BreadcrumbLink.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from '@mui/material'
+import classNames from 'classnames'
+
+function BreadcrumbLink({ linkItem, truncate }) {
+  return (
+    <Link
+      className={classNames('header__link', truncate && `header__link--truncated`)}
+      underline={linkItem?.path ? 'hover' : 'none'}
+      color="inherit"
+      href={linkItem?.path}>
+      <span className="header__link__text">{linkItem?.text}</span>
+
+      <span className="header__link__ellipsis">...</span>
+    </Link>
+  )
+}
+
+export default BreadcrumbLink
