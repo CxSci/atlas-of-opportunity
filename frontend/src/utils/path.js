2caa2cc248759d6fcf99d35be304b2d168e0517e
Leo Esaki <leo.esaki3@gmail.com>
2022-06-19 14:08:12 -0300

    Make 'View Comparison' button to go to comparison page route

--- frontend/src/utils/path.js
+++ frontend/src/utils/path.js
@@ -2,12 +2,12 @@ const PATH = {
   HOME: '/',
   DATASET: '/explore/:datasetId',
   DATASET_ENTRY: '/explore/:datasetId/:entryId',
-  COMPARISON: '/explore/:datasetId/comparison',
+  COMPARISON: '/explore/:datasetId/comparison/:comparisonEntryIds',
   GUIDED_TOOL: '/tools/:toolId',
   FAQ: '/faq',
   RESEARCH: '/research',
   CONTRIBUTORS: '/contributors',
-  404: '/404'
+  404: '/404',
 }
 
 export default PATH
